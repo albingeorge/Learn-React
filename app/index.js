c4385d74ad3e4204b1138af234d6b8d3ce36b518
Albin George <albinin0002@gmail.com>
2017-02-12 12:04:06 +0530

    Added twitter handles

--- app/index.js
+++ app/index.js
@@ -1,12 +1,26 @@
-// let React = require("react");
-// let ReactDOM = require("react-dom");
-
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+let User = React.createClass({
+    render: function() {
+        let twitter = (this.props.twitter) ? (this.renderTwitter(this.props.twitter)) : null;
+        return (<div>{this.props.name} : {twitter}</div>)
+    },
+    renderTwitter: function(twitter) {
+        return (<a href={'http://www.twitter.com/' + twitter} >Twitter</a>)
+    }
+})
+
+
 let App = React.createClass( {
     render: function() {
-        return <h1>Hello World 2</h1>
+        return (
+            <div>
+                <h1>User List</h1>
+                <User name="User 1" twitter="handle1"/>
+                <User name="User 2"/>
+            </div>
+        )
     }
 })
 
