68fc29ed961abf56213e45aa107f6ab51cd4671d
Albin George <albinin0002@gmail.com>
2017-02-15 12:27:32 +0530

    Routes

--- app/index.js
+++ app/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import axios from 'axios'
-
+import { Router, Route, browserHistory, Link } from 'react-router'
 
 // This is a stateless functional component
 let User = function(props) {
@@ -13,14 +13,14 @@ let User = function(props) {
 }
 
 
-let App = React.createClass( {
+let Users = React.createClass( {
     getInitialState: function() {
         return {
             users: []
         }
     },
     componentDidMount: function() {
-        axios.get('http://swapi.co/api/people').then(results => {
+        axios.get('http://swapi.co/api/people/').then(results => {
             this.setState({
                 users: results.data.results
             });
@@ -30,6 +30,7 @@ let App = React.createClass( {
         return (
             <div>
                 <h1>Star Wars Characters:</h1>
+                    <Link to="/other">Other</Link>
                 {
                     this.state.users.map(function(obj) {
                         return <User name={obj.name} key={obj.name} />;
@@ -40,4 +41,13 @@ let App = React.createClass( {
     }
 })
 
-ReactDOM.render(<App />, document.getElementById("root"));
+let Other = () => {
+    return <div>Other</div>
+}
+
+ReactDOM.render((
+    <Router history={browserHistory}>
+        <Route path="/" component={Users} />
+        <Route path="/other" component={Other} />
+    </Router>
+), document.getElementById("root"));
